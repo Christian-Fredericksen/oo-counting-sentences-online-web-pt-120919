1fd0def08e192ef86d5cb4e840826df99789b0c1
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-06 17:42:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
+  
 
   def question?
-
+    self.end_with?("?")
   end
+  
 
   def exclamation?
-
+    self.end_with?("!")
   end
+  
 
   def count_sentences
-
+    sentences = []
+    self.split(/\./).size 
   end
+    # sentences = []
+ 	  # self.split("").each do |char|
+ 		 # if char == "!" || char == "?"|| char == "."
+ 			# sentences << char
+ 		 # end
+ 	  # end
+ 	  # sentences.length
+    # end
+  
 end
